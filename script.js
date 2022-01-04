9c1e17e3049ccba0b38ef7b5991b3edf4ea716f3
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-01-03 20:17:35 -0600

    buttons

--- script.js
+++ script.js
@@ -46,24 +46,29 @@ const currentOperandTextElement = document.querySelector('[data-current-operand]
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
     })
   })
   
   operationButtons.forEach(button => {
     button.addEventListener('click', () => {
-
+      calculator.chooseOperation(button.innerText)
+      calculator.updateDisplay()
     })
   })
   
   equalsButton.addEventListener('click', button => {
-
+    calculator.compute()
+    calculator.updateDisplay()
   })
   
   allClearButton.addEventListener('click', button => {
-
+    calculator.clear()
+    calculator.updateDisplay()
   })
   
   deleteButton.addEventListener('click', button => {
-
+    calculator.delete()
+    calculator.updateDisplay()
   })
