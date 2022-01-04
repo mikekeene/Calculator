632b12478de36e9934a6c5c1f0415d30b1f6883e
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-01-03 20:17:07 -0600

    buttons

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear(){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNmuber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    getDisplayNumber(number) {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -5,3 +43,27 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+
+    })
+  })
+  
+  operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+
+    })
+  })
+  
+  equalsButton.addEventListener('click', button => {
+
+  })
+  
+  allClearButton.addEventListener('click', button => {
+
+  })
+  
+  deleteButton.addEventListener('click', button => {
+
+  })
